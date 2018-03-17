bf18e9f0365322003310147cab4dbd0015f61788
rzhou1993 <rzhou4@binghamton.edu>
2018-03-17 13:18:46 -0400

    first commit

---
+++ Entity_Resolution.py
@@ -0,0 +1,31 @@
+import json
+import csv
+
+"""
+This assignment can be done in groups of 3 students. Everyone must submit individually.
+
+Write down the UNIs of your group (if applicable)
+
+Name : your name 
+Uni  : your uni
+
+Member 2: name, uni
+
+Member 3: name, uni
+"""
+
+
+def get_matches(locu_train_path, foursquare_train_path, matches_train_path, locu_test_path, foursquare_test_path):
+    """
+        In this function, You need to design your own algorithm or model to find the matches and generate
+        a matches_test.csv in the current folder.
+
+        you are given locu_train, foursquare_train json file path and matches_train.csv path to train
+        your model or algorithm.
+
+        Then you should test your model or algorithm with locu_test and foursquare_test json file.
+        Make sure that you write the test matches to a file in the same directory called matches_test.csv.
+
+    """
+    pass
+
