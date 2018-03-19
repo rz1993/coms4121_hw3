de733cf0d92889a432b26ce60f77bf62a78c3af7
rzhou1993 <rzhou4@binghamton.edu>
2018-03-18 20:55:47 -0400

    Added prefiltering: test f1: 97.03

--- Entity_Resolution.py
+++ Entity_Resolution.py
@@ -7,7 +7,7 @@ import re
 from collections import Counter
 from fuzzywuzzy import fuzz
 import lightgbm as lgb
-from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import f1_score
 from sklearn.feature_extraction.text import CountVectorizer
 
 """
@@ -15,12 +15,12 @@ This assignment can be done in groups of 3 students. Everyone must submit indivi
 
 Write down the UNIs of your group (if applicable)
 
-Name : your name
-Uni  : your uni
+Name : Roland Zhou
+Uni  : rz2388
 
-Member 2: name, uni
+Member 2: Mohammad Radiyat, mr3719
 
-Member 3: name, uni
+Member 3: Kathy Lin, kl2615
 """
 def get_matches(locu_train_path, foursquare_train_path, matches_train_path, locu_test_path, foursquare_test_path):
     """
@@ -34,26 +34,32 @@ def get_matches(locu_train_path, foursquare_train_path, matches_train_path, locu
         Make sure that you write the test matches to a file in the same directory called matches_test.csv.
 
     """
-
     # Load the training data as pandas dataframes
     matches_train = pd.read_csv(matches_train_path)
     foursq_train = pd.read_json(foursquare_train_path)
     locu_train = pd.read_json(locu_train_path)
 
     # Join the foursq and locu data using a cartesian product
-    train_df = crossjoin(foursq_train, locu_train, suffixes=['_foursq', '_locu'])
+    train_df = crossjoin(foursq_train,
+        locu_train,
+        suffixes=['_foursq', '_locu'])
 
     # Merge the match labels and training data
     matches_train.columns = ['id_locu', 'id_foursq']
     matches_train['matched'] = 1
-    train_df = train_df.merge(matches_train, how='left', left_on=['id_locu', 'id_foursq'], right_on=['id_locu', 'id_foursq'])
+    train_df = train_df.merge(matches_train,
+        how='left',
+        left_on=['id_locu', 'id_foursq'],
+        right_on=['id_locu', 'id_foursq'])
     train_df['matched'].fillna(0, inplace=True)
 
     foursq_test = pd.read_json(foursquare_test_path)
-    locu_test = pd.read_json(locu_train_path)
+    locu_test = pd.read_json(locu_test_path)
 
     # Join the foursq and locu data using a cartesian product
-    test_df = crossjoin(foursq_test, locu_test, suffixes=['_foursq', '_locu'])
+    test_df = crossjoin(foursq_test,
+        locu_test,
+        suffixes=['_foursq', '_locu'])
 
     # Gathering common brand names based on counts
     all_names = foursq_train['name'] \
@@ -67,6 +73,7 @@ def get_matches(locu_train_path, foursquare_train_path, matches_train_path, locu
     def name_match(df):
         return name_match_no_brands(df, brand_names)
 
+    '''
     features_train = featurize(train_df,
                                lat_long_continuous,
                                address_match,
@@ -74,6 +81,7 @@ def get_matches(locu_train_path, foursquare_train_path, matches_train_path, locu
                                name_fuzzy,
                                addr_fuzzy,
                                phone_num_match)
+
     features_test = featurize(test_df,
                               lat_long_continuous,
                               address_match,
@@ -81,21 +89,54 @@ def get_matches(locu_train_path, foursquare_train_path, matches_train_path, locu
                               name_fuzzy,
                               addr_fuzzy,
                               phone_num_match)
+    '''
+
+    df_train, _ = filter_matches(train_df,
+        lambda df: simple_addr_match(df) & name_match(df))
+    df_test, filtered_matches = filter_matches(test_df,
+        lambda df: simple_addr_match(df) & name_match(df))
+
+    df_train = filter_neg(df_train,
+        lambda df: ~postal_match(df) & ~region_match(df))
+    df_test = filter_neg(df_test,
+        lambda df: ~postal_match(df) & ~region_match(df))
+
+    feats_train = featurize(df_train,
+                            lat_long_continuous,
+                            name_fuzzy,
+                            addr_fuzzy,
+                            phone_num_match)
+
+    feats_test = featurize(df_test,
+                           lat_long_continuous,
+                           name_fuzzy,
+                           addr_fuzzy,
+                           phone_num_match)
 
     '''
     Load and train a gradient boosted tree using LightGBM's
     default configuration and Dataset wrapper API.
     '''
-    features_train_lgb = lgb.Dataset(features_train, label=train_df['matched'])
+    feats_train_lgb = lgb.Dataset(feats_train, label=df_train['matched'])
     params = {}
-    clf = lgb.train(params, features_train_lgb, 100)
+    clf = lgb.train(params, feats_train_lgb, 100)
+
+    fake_preds = [True] * _.shape[0]
+    print('F1 score: {}'.format(
+        f1_score(np.concatenate([clf.predict(feats_train) > .35, fake_preds]),
+                 np.concatenate([df_train['matched'].values > 0, fake_preds]))
+    ))
 
     # Binarize continuous prediction scores from 0 to 1
-    preds_test = clf.predict(features_test)
-    preds_test = preds_test > 0.35
+    preds_test = clf.predict(feats_test) > .35
+    print("Shapes:")
+    print(feats_test.shape)
+    print(preds_test.shape)
+    print(df_test.shape)
 
     # Select rows which are matches via boolean indexing and write to csv
-    matches_test = test_df.loc[preds_test][['id_locu', 'id_foursq']]
+    matches_test = df_test[preds_test][['id_locu', 'id_foursq']]
+    matches_test = pd.concat([filtered_matches[['id_locu', 'id_foursq']], matches_test])
     matches_test.columns = ['locu_id', 'foursquare_id']
     matches_test.to_csv('matches_test.csv', index=False)
 
@@ -110,7 +151,6 @@ def crossjoin(df1, df2, **kwargs):
 '''
 Text processing
 '''
-
 stop_words = ['restaurant', 'inc', 'cafe', 'bakery', 'and', 'the', 'of']
 
 def remove_words(s, words):
@@ -156,6 +196,12 @@ def phone_num_match(df):
 
     norm_phone = df['phone_foursq'].apply(lambda p: norm_foursq_phone(p, default='None_foursq'))
     return norm_phone == df['phone_locu'].apply(lambda p: norm_foursq_phone(p, default='None_locu'))
+
+def simple_addr_match(df):
+    '''Simple address exact matching for filtering purposes'''
+    return ~(df['street_address_locu'] == '') \
+        & ~(df['street_address_foursq'] == '') \
+        & (df['street_address_locu'] == df['street_address_foursq'])
 
 def address_match(df):
     def norm(a, default='None'):
@@ -210,6 +256,60 @@ def lat_long_continuous(df):
                         - norm_coord(row['longitude_locu']))**2)
 
     return df.apply(euclid_dist, axis=1)
+
+'''
+Filtering on pairs to avoid training model on all n^2 pairs
+'''
+def filter_matches(df, *high_prec_feats):
+    '''
+    Filter out pairs with high precision features.
+    Since each location can only be matched once, for each match
+    we can reduce all other negative pairs containing that location.
+    '''
+    # Accumulate the union of positive predictions according to our feats
+    # These can be filtered out by virtue of our feats being high precision
+    pos = pd.Series([False] * df.shape[0])
+    for feat in high_prec_feats:
+        pos |= feat(df)
+
+    # Select the positively predicted pairs
+    matches = df[pos]
+
+    # Remove all locu and foursquare ids that have already been matched
+    # Do this using np.array operations and boolean indexing (which may not be the most efficient)
+    keep = pd.Series([True] * (df.shape[0] - matches.shape[0]))
+    for id in matches['id_locu']:
+        keep = keep & (df['id_locu'] != id)
+
+    for id in matches['id_foursq']:
+        keep = keep & (df['id_foursq'] != id)
+
+    # Return the positive predictions separately
+    return df[~pos & keep], matches
+
+def filter_neg(df, *high_rec_feats):
+    '''
+    Filter out pairs that do not match against very rec features.
+    This means we can exclude them from consideration because they are
+    unlikely to match.
+    '''
+    excl = pd.Series([False] * df.shape[0])
+    for feat in high_rec_feats:
+        excl |= feat(df)
+
+    return df[~excl]
+
+'''Evaluating precision and recall of potentially simple filtering heuristics'''
+def postal_match(df):
+    return df['postal_code_locu'] == df['postal_code_foursq']
+
+def region_match(df):
+    return df['locality_locu'] == df['locality_foursq']
+
+def addr_match(df):
+    return ~(df['street_address_locu'] == '') \
+        & ~(df['street_address_foursq'] == '') \
+        & (df['street_address_locu'] == df['street_address_foursq'])
 
 '''Creating the feature matrix'''
 
